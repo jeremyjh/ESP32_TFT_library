b6cf29358fc42e7ad5679c875535a05aada654b7
James Hawtin <oolon@ankh.org>
2020-07-04 01:50:47 +0100

    Adding support for the TTGO [T4] T-WATCHER v 1.3

--- components/tft/tftspi.h
+++ components/tft/tftspi.h
@@ -193,6 +193,7 @@ extern "C" {
 
 #elif CONFIG_TFT_PREDEFINED_DISPLAY_TYPE == 6
 //CONFIG FOR TTGO T-WRISTBAND
+// ---------------------------------------------------------
 #define DEFAULT_DISP_TYPE           DISP_TYPE_ST7735R
 #define DEFAULT_TFT_DISPLAY_WIDTH   80
 #define DEFAULT_TFT_DISPLAY_HEIGHT  160
@@ -223,9 +224,37 @@ extern "C" {
 #define PIN_NUM_BCKL 27         // GPIO used for backlight control
 #define PIN_BCKL_ON   1     // GPIO value for backlight ON
 #define PIN_BCKL_OFF  0     // GPIO value for backlight OFF
-//END TTGO T_DISPLAY
-#else
+//END TTGO T-WRISTBAND
 
+#elif CONFIG_TFT_PREDEFINED_DISPLAY_TYPE == 7
+// CONFIG FOR TTGO [T4] T-WATCHER v1.3
+// ---------------------------------------------------------
+#define DEFAULT_DISP_TYPE   DISP_TYPE_ILI9341
+#define DEFAULT_TFT_DISPLAY_WIDTH   240
+#define DEFAULT_TFT_DISPLAY_HEIGHT  320
+#define DISP_COLOR_BITS_24          0x66
+#define DEFAULT_GAMMA_CURVE         0
+#define DEFAULT_SPI_CLOCK           26000000
+#define TFT_INVERT_ROTATION         0
+#define TFT_INVERT_ROTATION1        0
+#define TFT_RGB_BGR                 0x08
+
+#define USE_TOUCH                   TOUCH_TYPE_NONE
+
+#define PIN_NUM_MISO 12		// SPI MISO
+#define PIN_NUM_MOSI 23		// SPI MOSI
+#define PIN_NUM_CLK  18		// SPI CLOCK pin
+#define PIN_NUM_CS   27		// Display CS pin
+#define PIN_NUM_DC   32		// Display command/data pin
+#define PIN_NUM_TCS   0		// Touch screen CS pin (NOT used if USE_TOUCH=0)
+
+#define PIN_NUM_RST   5  	// GPIO used for RESET control (#16)
+#define PIN_NUM_BCKL  4  	// GPIO used for backlight control
+#define PIN_BCKL_ON   1  	// GPIO value for backlight ON
+#define PIN_BCKL_OFF  0  	// GPIO value for backlight OFF
+// END TTGO T-WATCHER
+
+#else
 // Configuration for other boards, set the correct values for the display used
 //----------------------------------------------------------------------------
 #define DISP_COLOR_BITS_24	0x66
